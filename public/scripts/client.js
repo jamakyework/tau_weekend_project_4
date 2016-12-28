853b281e837291fa3e7fc566593db9e12021fe52
John Amakye <john.amakye@minneapolismn.gov>
2016-12-28 13:52:11 -0600

    cleaned up readme

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,122 +1,151 @@
 //start $
-$(document).ready(function(){
-  console.log("$");
+$(document).ready(function() {
+    console.log("$");
 
-  //-----------Buttons------------------ //
-  //start createTaskBtn click
-  $("#createTaskBtn").on('click', function(){
-    console.log("createTaskBtn clicked");
-    //run post
-    postTask();
-    //run get
-    getTask();
-    //clear Input
-    $("#taskInput").val("");
-  });//end createTaskBtn click
+    //-----------Buttons------------------ //
+    //start createTaskBtn click
+    $("#createTaskBtn").on('click', function() {
+        console.log("createTaskBtn clicked");
+        //run post
+        postTask();
+        //run get
+        getTask();
+        //clear Input
+        $("#taskInput").val("");
+    }); //end createTaskBtn click
 
-  //start displayTaskBtn
-  $("#displayTasksBtn").on('click',function() {
-    console.log("displayTasksBtn clicked");
-    getTask();
-  });//end displayTaskBtn
+    //start displayTaskBtn
+    $("#displayTasksBtn").on('click', function() {
+        console.log("displayTasksBtn clicked");
+        getTask();
+    }); //end displayTaskBtn
 
-  //start completeTaskBtn
-  $('#outputDiv').on('click', '.complete-task-btn',function(){
-    var id= $(this).attr('data');
-    console.log("completeTaskBtn clicked", id);
-    var objectToSend = {
-      id: id
-    };
-    $.ajax({
-      type:'PUT',
-      url:'/putTask',
-      data: objectToSend,
-      success:function(response){
-        console.log(response);
-      }
-    });//end ajax
-    getTask();
-  });//end completeTaskBtn
-
-  //start deleteTaskBtn
-  $('#outputDiv').on('click', '.delete-task-btn',function(){
-    var id= $(this).attr('data');
-    console.log("deleteTaskBtn clicked", id);
-    var objectToSend = {
-          id: id
+    //start completeTaskBtn
+    $('#outputDiv').on('click', '.complete-task-btn', function() {
+        var id = $(this).attr('data');
+        console.log("completeTaskBtn clicked", id);
+        var objectToSend = {
+            id: id
         };
         $.ajax({
-          type:'DELETE',
-          url:'/deleteTask',
-          data: objectToSend,
-          success:function(response){
-            console.log("delete response: ", response);
-            var responseArray = [response];
-            console.log("delete response.length: ", responseArray.length);
-          },//end success
+            type: 'PUT',
+            url: '/putTask',
+            data: objectToSend,
+            success: function(response) {
+                    console.log("in complete task click:", response);
+                } //end success
         }); //end ajax
-         getTask();
-  }); //end deleteTaskBtn
+        getTask();
+        changecolor();
+    }); //end completeTaskBtn
 
-  //--------------AJAX Calls-------------//
-  //postTask (send user input)
-  var postTask = function(){
-    console.log('in postTask');
-    //assemble userInput
-    var taskInputObj={
-      task: $("#taskInput").val(),
-      status: $(".status-drop-down").val(),
-      complete: false,
-    };//end taskInputObj
-    //start ajax
-    $.ajax({
-      type: 'POST',
-      url: '/postTask',
-      data: taskInputObj,
-      success: function( response ){
-        console.log( 'back from post call:', response );
-      }, // end success
-      error: function(){
-        console.log( 'error with ajax call...');
-      } // end error
-    }); // end ajax
-  }; // end postTask
+    //start deleteTaskBtn
+    $('#outputDiv').on('click', '.delete-task-btn', function() {
+        var id = $(this).attr('data');
+        console.log("deleteTaskBtn clicked", id);
+        var objectToSend = {
+            id: id
+        };
+        $.ajax({
+            type: 'DELETE',
+            url: '/deleteTask',
+            data: objectToSend,
+            success: function(response) {
+                console.log("delete response: ", response);
+            }, //end success
+        }); //end ajax
+        getTask();
+    }); //end deleteTaskBtn
 
-  //getTask (get back info from server/DB)
-  var getTask = function(){
-    console.log( 'in getTask' );
-    $.ajax({
-      type: 'GET',
-      url: '/getTask',
-      success: function( response ){
-        console.log( 'back from GET call:', response );
-        console.log('GET response.length', response.length);
-        //start if
-        if (response.length === 0) {
-          $( '#outputDiv' ).html( "<p>No tasks in to do list</p>" );
-        }//end if
-        else {
-        displayTask( response );
-      }//end else
-      },// end success
-      error: function(){
-        console.log( 'error with ajax call...');
-      } // end error
-    }); // end ajax
-  }; // end getTask
+    //------------AJAX Calls-------------//
+    //postTask (send user input)
+    var postTask = function() {
+        console.log('in postTask');
+        //assemble userInput
+        var taskInputObj = {
+            task: $("#taskInput").val(),
+            status: $(".status-drop-down").val(),
+            complete: false,
+        }; //end taskInputObj
+        //start ajax
+        $.ajax({
+            type: 'POST',
+            url: '/postTask',
+            data: taskInputObj,
+            success: function(response) {
+                console.log('back from post call:', response);
+            }, // end success
+            error: function() {
+                    console.log('error with ajax call...');
+                } // end error
+        }); // end ajax
+    }; // end postTask
 
-  //------------Display Function----------//
-  //displayTask
-  var displayTask = function(tasks){
-    console.log( 'in displayTask:', tasks );
-    // loop through the todolist and display each task
-    var outputText = '';
-    for (var i = 0; i < tasks.length; i++) {
-      console.log("status for loop: ", tasks[i].status);
-      console.log("tasks[i].id:", tasks[i].id);
-      outputText += "<ul>Task: " + tasks[i].task + "</ul><ul>Status: " + tasks[i].status + "</ul><ul>" + "<button class='complete-task-btn'  type='button' name='completetask' data=" + tasks[i].id + "> Complete Task </button>" + " " + "<button class='delete-task-btn' type='button' name='deleteTask' data=" + tasks[i].id + ">Delete Task</button></ul>";
-    } // end for
-    $( '#outputDiv' ).html( outputText );
-  };//end displayOnDom
+    //getTask (get back info from server/DB)
+    var getTask = function() {
+        console.log('in getTask');
+        $.ajax({
+            type: 'GET',
+            url: '/getTask',
+            success: function(response) {
+                console.log('back from GET call:', response);
+                console.log('GET response.length', response.length);
+                //start if
+                if (response.length === 0) {
+                    $('#outputDiv').html("<p> No tasks in to do list </p>");
+                } //end if
+                else {
+                    displayTask(response);
+                } //end else
+            }, // end success
+            error: function() {
+                    console.log('error with ajax call...');
+                } // end error
+        }); // end ajax
+    }; // end getTask
 
-});// end $
+    //------------Functions----------//
+    //displayTask
+    var displayTask = function(tasks) {
+        console.log('in displayTask:', tasks);
+        // loop through the todolist and display each task
+        var outputText = '';
+        for (var i = 0; i < tasks.length; i++) {
+            // console.log("status for loop: ", tasks[i].status);
+            // console.log("tasks[i].id:", tasks[i].id);
+            outputText += "<div class='task-display'><ul>Task: " + tasks[i].task + "</ul><ul>Status: " + tasks[i].status + "</ul></div>";
+            outputText += "<div><ul><button class='complete-task-btn'  type='button' name='completetask' data=" + tasks[i].id + "> Complete Task </button>" + " " + "<button class='delete-task-btn' type='button' name='deleteTask' data=" + tasks[i].id + ">Delete Task</button></ul></div>";
+        } // end for
+        $('#outputDiv').html(outputText);
+    }; //end displayOnDom
+
+    var changeColor = function() {
+        $(".task-display").css("background-color", "green");
+    };
+
+}); // end $
+
+// //displayTask
+// var displayTask = function(tasks) {
+//     console.log('in displayTask:', tasks);
+//     // loop through the todolist and display each task
+//     var outputText = '';
+//     for (var i = 0; i < tasks.length; i++) {
+//         console.log("status for loop: ", tasks[i].status);
+//         console.log("tasks[i].id:", tasks[i].id);
+//         if (task[i].status === "complete") {
+//
+//
+//         } else {
+//
+//         }
+//
+//     } // end for
+//     $('#outputDiv').html(outputText);
+// }; //end displayOnDom
+
+// if (confirm("do you want to delete this task?")==="true") {
+//   } else { getTask();}
+
+//confirm("do you want to delete this task?");
+//on complete-task-btn click i want to change the color of the output to Green
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-Weekend Challenge 4: The To Do List
-Hello Primers!
+Weekend Challenge 4: The To Do List Hello Primers!
 
 Welcome to your 4th weekend challenge! Full stack is pretty awesome huh? The idea in such a short time, you are able to spin up a full application architecture is pretty incredible. Also worth noting, there are only two weekend challenges left!
 
@@ -7,35 +6,23 @@ This weekend is all about showing us that you have a handle on each of the diffe
 
 Here are the specific components for the challenge:
 
-[x]Create a front end experience that allows a user to create a task.
-[x]When the task is created, it should be stored inside of a database (SQL)
-[]Whenever a task is created the front end should refresh to show all tasks that need to be completed.
-[]Each task should have an option to 'Complete' or 'Delete'.
-When a task is complete, its visual representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete)
-Whether or not a task is complete should also be stored in the database.
-Deleting a task should remove it both from the Front End as well as the Database.
-Make sure that you also show us your best styling chops. We encourage you to try and write pure CSS rather than use Bootstrap.
+[x]Create a front end experience that allows a user to create a task. [x]When the task is created, it should be stored inside of a database (SQL) []Whenever a task is created the front end should refresh to show all tasks that need to be completed. []Each task should have an option to 'Complete' or 'Delete'. When a task is complete, its visual representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete) Whether or not a task is complete should also be stored in the database. Deleting a task should remove it both from the Front End as well as the Database. Make sure that you also show us your best styling chops. We encourage you to try and write pure CSS rather than use Bootstrap.
 
 We would recommend you spend some time thinking about how to approach this problem. Think through all the logic that will be needed prior to writing any code. Take your time, relax, remember that impostor syndrome is real, and that you are capable of knocking this out of the park!
 
 Additionally, please include some way to recreate your initial database schema. This can be a .sql file with CREATE TABLE statements or you can create your schema automatically when your app loads.
 
-HARD MODE
-=========
+# HARD MODE
 
 In whatever fashion you would like, create an 'are you sure: yes / no' option when deleting a task. Once again, you can interrupt this however you would like.
 
-PRO MODE
-========
+# PRO MODE
 
 Adjust the logic so that completed tasks are brought to the bottom of the page, where the remaining tasks left to complete are brought to the top of the list.
-
-
+================================================================================
 Project Setup
-=============
 
-Required Technologies
-=====================
+# Required Technologies
 [x] HTML
 [x] CSS
 [x] JavaScript
@@ -45,63 +32,39 @@ Required Technologies
 [x] body-parser
 [x] pSQL
 
-Source and connect required files and technologies
-==================================================
+# Source and connect required files and technologies
 [x] Create project folder
 [x] Connect public/style/style/style.css
 [x] Connect scripts/client.js
 [x] Connect scripts/vendors/jquery
 [x] Create gitignore, git init, git add ., git commit, git push,
-[x] npm init, npm install express,  body-parser, pg, jquery,
+[x] npm init, npm install express, body-parser, pg, jquery,
 
-=================================================================================================
-
-
+================================================================================
 Project Tasks
-=============
 
-index.html
-=========
+# index.html
 [x] Create a front end experience that allows a user to create a task
-    [x] Create public/views/index.html
-    [x] Text Input and Create Task button
-    [x] display/outputDIV
+[x] Create public/views/index.html
+[x] Text Input and Create Task button
+[x] display/outputDIV
 [x] In output, need to reflect status of task and option to complete task or delete task
 
-client.js
-=========
-    [x] Create scripts/client.js
-    [x] Connect userinput from index.html to client.js
-    [x] On button click trigger AJAX post call to send data from HTML input from client.js to app.js
-    [x] On button click trigger AJAX get call to retrieve data from app.js to client
-    [x] Output display data from client.js to DOM,
-    [x] Display tasks in ul,
-    [] Connect status (drop down?) in outputDiv to  logic (eg if status = x, perform x, delete, change status etc)
+# client.js
+[x] Create scripts/client.js
+[x] Connect userinput from index.html to client.js
+[x] On button click trigger AJAX post call to send data from HTML input from client.js to app.js
+[x] On button click trigger AJAX get call to retrieve data from app.js to client [x] Output display data from client.js to DOM,
+[x] Display tasks in ul,
+[] Connect status (drop down?) in outputDiv to logic (eg if status = x, perform x, delete, change status etc)
 
+# app.js
+[x] Create server/app.js with requirements express, body-parser, pg, jquery, connectionString [x] Connect client.js to app.js [x] Create connection string to connect DB to app.js, [x] Retrieve data from DB to app.js [x] Retrieve data from app.js to client.js
 
-app.js
-======
-    [x] Create server/app.js with requirements express, body-parser, pg, jquery,
-    connectionString
-    [x] Connect client.js to app.js
-    [x] Create connection string to connect DB to app.js,
-    [x] Retrieve data from DB to app.js
-    [x] Retrieve data from app.js to client.js
+# postico
+[x] When the task is created, it should be stored inside of a database (SQL) [x] Create Table in postico (use pSql) [x] Whether or not a task is complete should also be stored in the database. [x] In table include task complete column set to boolean [x] Deleting a task should remove it both from the Front End as well as the Database. (connect delete button from index to client to app to db to perform Delete option of CRUD)
 
-postico
-=======
-[x] When the task is created, it should be stored inside of a database (SQL)
-    [x] Create Table in postico (use pSql)
-[x] Whether or not a task is complete should also be stored in the database.
-    [x] In table include task complete column set to boolean
-    [] Deleting a task should remove it both from the Front End as well as the Database. (connect delete button from index to client to app to db to perform Delete option of CRUD)
-
-style.css
-=========
-[] Whenever a task is created the front end
-   should refresh to show all tasks that need to be completed.(on Create Task button click .hide, .show maybe?)
-[] When a task is complete, its visual
-   representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete)
-[] Make sure that you also show us your best
-   styling chops. We encourage you to try and write pure CSS rather than use Bootstrap.
-=================================================================================================
+# style.css
+[] Whenever a task is created the front end should refresh to show all tasks that need to be completed.(on Create Task button click .hide, .show maybe?)
+[] When a task is complete, its visual representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete)
+[] Make sure that you also show us your best styling chops. We encourage you to try and write pure CSS rather than use Bootstrap.
